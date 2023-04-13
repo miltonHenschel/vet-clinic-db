5e5fc9018e5435432a422f0ebe4c0c33dab2c6f9
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-13 16:39:15 +0100

    Add queries

--- queries.sql
+++ queries.sql
@@ -50,3 +50,15 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO delete;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) AS nbr_animals FROM animals;
+
+SELECT COUNT(*) AS nbr_animals FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) AS avr_weight_kg FROM animals;
+
+SELECT neutered, SUM(escape_attempts) total_attempts FROM animals GROUP BY neutered;
+
+SELECT species, MIN(weight_kg) min_weight_kg, MAX(weight_kg) max_weight_kg FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) avr_escape_attempts FROM animals WHERE EXTRACT(year FROM date_of_birth) BETWEEN 1990 AND 2000 GROUP BY species;
