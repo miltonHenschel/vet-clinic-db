9070876bbb1ecf67bd72f8bbb52b21b02877f15d
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-13 05:07:58 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -12,19 +12,19 @@ VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES ('Devimon', '2017-05-12', 11, true, 5);
 
-// Add new records
+/* Add new records */
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
-VALUES ('Charmander','2020-02-08',11,false,0);
+VALUES ('Charmander','2020-02-08',-11,false,0);
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
-VALUES ('Plantmon','2021-11-15',5.7,true,2);
+VALUES ('Plantmon','2021-11-15',-5.7,true,2);
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
-VALUES ('Squirtle','1993-04-02',12.13,false,3);
+VALUES ('Squirtle','1993-04-02',-12.13,false,3);
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
-VALUES ('Angemon','2005-06-12',45,true,1);
+VALUES ('Angemon','2005-06-12',-45,true,1);
 
 INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
 VALUES ('Boarmon','2005-06-07',20.4,true,7);
