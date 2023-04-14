660a1e7bc991ba976728c78da8b211c3ea1f15fd
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-14 01:25:12 +0100

    create owners & species & add fk to animals tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD species text;
+
+/* update */
+
+ALTER TABLE animals ADD COLUMN species text;
+
+CREATE TABLE owners (
+	id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	full_name text,
+	age int
+);
+
+CREATE TABLE species (
+	id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	name text
+);
+
+ALTER TABLE animals DROP species;
+
+ALTER TABLE animals ADD COLUMN species_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
